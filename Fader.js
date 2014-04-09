c6fb0ee3458b957483d91b1bddc59a215194a155
dmvaldman <dmvaldman@gmail.com>
2014-04-09 10:50:38 -0700

    initial commit

---
+++ Fader.js
@@ -0,0 +1,122 @@
+define(function(require, exports, module) {
+    var Transitionable = require('famous/transitions/Transitionable');
+    var OptionsManager = require('famous/core/OptionsManager');
+
+    /**
+     * Modifier that allows you to fade the opacity of affected renderables in and out.
+     * @class Fader
+     * @constructor
+     * @param {Object} [options] options configuration object.
+     * @param {Boolean} [options.cull=false] Stops returning affected renderables up the tree when they're fully faded when true.
+     * @param {Transition} [options.transition=true] The main transition for showing and hiding.
+     * @param {Transition} [options.pulseInTransition=true] Controls the transition to a pulsed state when the Fader instance's pulse
+     * method is called.
+     * @param {Transition} [options.pulseOutTransition=true]Controls the transition back from a pulsed state when the Fader instance's pulse
+     * method is called.
+     *
+     */
+    function Fader(options, startState) {
+        this.options = Object.create(Fader.DEFAULT_OPTIONS);
+        this._optionsManager = new OptionsManager(this.options);
+
+        if (options) this.setOptions(options);
+
+        if (!startState) startState = 0;
+        this.transitionHelper = new Transitionable(startState);
+    }
+
+    Fader.DEFAULT_OPTIONS = {
+        cull: false,
+        transition: true,
+        pulseInTransition: true,
+        pulseOutTransition: true
+    };
+
+    /**
+     * Set internal options, overriding any default options
+     *
+     * @method setOptions
+     *
+     * @param {Object} [options] overrides of default options.  See constructor.
+     */
+    Fader.prototype.setOptions = function setOptions(options) {
+        return this._optionsManager.setOptions(options);
+    };
+
+    /**
+     * Fully displays the Fader instance's associated renderables.
+     *
+     * @method show
+     * @param {Transition} [transition] The transition that coordinates setting to the new state.
+     * @param {Function} [callback] A callback that executes once you've transitioned to the fully shown state.
+     */
+    Fader.prototype.show = function show(transition, callback) {
+        transition = transition || this.options.transition;
+        this.set(1, transition, callback);
+    };
+
+    /**
+     * Fully fades the Fader instance's associated renderables.
+     *
+     * @method hide
+     * @param {Transition} [transition] The transition that coordinates setting to the new state.
+     * @param {Function} [callback] A callback that executes once you've transitioned to the fully faded state.
+     */
+    Fader.prototype.hide = function hide(transition, callback) {
+        transition = transition || this.options.transition;
+        this.set(0, transition, callback);
+    };
+
+    /**
+     * Manually sets the opacity state of the fader to the passed-in one. Executes with an optional
+     * transition and callback.
+     *
+     * @method set
+     * @param {Number} state A number from zero to one: the amount of opacity you want to set to.
+     * @param {Transition} [transition] The transition that coordinates setting to the new state.
+     * @param {Function} [callback] A callback that executes once you've finished executing the pulse.
+     */
+    Fader.prototype.set = function set(state, transition, callback) {
+        this.halt();
+        this.transitionHelper.set(state, transition, callback);
+    };
+
+    /**
+     * Halt the transition
+     *
+     * @method halt
+     */
+    Fader.prototype.halt = function halt() {
+        this.transitionHelper.halt();
+    };
+
+    /**
+     * Tells you if your Fader instance is above its visibility threshold.
+     *
+     * @method isVisible
+     * @return {Boolean} Whether or not your Fader instance is visible.
+     */
+    Fader.prototype.isVisible = function isVisible() {
+        return (this.transitionHelper.get() > 0);
+    };
+
+    /**
+     * Return render spec for this Modifier, applying to the provided
+     *    target component.  This is similar to render() for Surfaces.
+     *
+     * @private
+     * @method modify
+     *
+     * @param {Object} target (already rendered) render spec to
+     *    which to apply the transform.
+     * @return {Object} render spec for this Modifier, including the
+     *    provided target
+     */
+    Fader.prototype.modify = function modify(target) {
+        var currOpacity = this.transitionHelper.get();
+        if (this.options.cull && !currOpacity) return undefined;
+        else return {opacity: currOpacity, target: target};
+    };
+
+    module.exports = Fader;
+});
---
+++ ModifierChain.js
@@ -0,0 +1,70 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Owner: david@famo.us
+ * @license MPL 2.0
+ * @copyright Famous Industries, Inc. 2014
+ */
+
+define(function(require, exports, module) {
+
+    /**
+     * A class to add and remove a chain of modifiers
+     *   at a single point in the render tree
+     *
+     * @class ModifierChain
+     * @constructor
+     */
+    function ModifierChain() {
+        this._chain = [];
+        if (arguments.length) this.add.apply(this, arguments);
+    }
+
+    /**
+     * Add a modifier, or comma separated modifiers, to the modifier chain.
+     *
+     * @method addModifier
+     *
+     * @param {...Modifier*} varargs args list of Modifiers
+     */
+    ModifierChain.prototype.addModifier = function addModifier(varargs) {
+        Array.prototype.push.apply(this._chain, arguments);
+    };
+
+    /**
+     * Remove a modifier from the modifier chain.
+     *
+     * @method removeModifier
+     *
+     * @param {Modifier} modifier
+     */
+    ModifierChain.prototype.removeModifier = function removeModifier(modifier) {
+        var index = this._chain.indexOf(modifier);
+        if (index < 0) return;
+        this._chain.splice(index, 1);
+    };
+
+    /**
+     * Return render spec for this Modifier, applying to the provided
+     *    target component.  This is similar to render() for Surfaces.
+     *
+     * @private
+     * @method modify
+     *
+     * @param {Object} input (already rendered) render spec to
+     *    which to apply the transform.
+     * @return {Object} render spec for this Modifier, including the
+     *    provided target
+     */
+    ModifierChain.prototype.modify = function modify(input) {
+        var chain  = this._chain;
+        var result = input;
+        for (var i = 0; i < chain.length; i++) {
+            result = chain[i].modify(result);
+        }
+        return result;
+    };
+
+    module.exports = ModifierChain;
+});
