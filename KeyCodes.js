8fcf06a00455a6f4cddfb01e46e114441093c97c
dmvaldman <dmvaldman@gmail.com>
2014-04-09 10:57:25 -0700

    initial commit

---
+++ KeyCodes.js
@@ -0,0 +1,92 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Owner: mark@famo.us
+ * @license MPL 2.0
+ * @copyright Famous Industries, Inc. 2014
+ */
+
+define(function(require, exports, module) {
+
+    /**
+     * Collection to map keyboard codes in plain english
+     *
+     * @class KeyCodes
+     * @static
+     */
+    var KeyCodes = {
+        0 : 48,
+        1 : 49,
+        2 : 50,
+        3 : 51,
+        4 : 52,
+        5 : 53,
+        6 : 54,
+        7 : 55,
+        8 : 56,
+        9 : 57,
+        a : 97,
+        b : 98,
+        c : 99,
+        d : 100,
+        e : 101,
+        f : 102,
+        g : 103,
+        h : 104,
+        i : 105,
+        j : 106,
+        k : 107,
+        l : 108,
+        m : 109,
+        n : 110,
+        o : 111,
+        p : 112,
+        q : 113,
+        r : 114,
+        s : 115,
+        t : 116,
+        u : 117,
+        v : 118,
+        w : 119,
+        x : 120,
+        y : 121,
+        z : 122,
+        A : 65,
+        B : 66,
+        C : 67,
+        D : 68,
+        E : 69,
+        F : 70,
+        G : 71,
+        H : 72,
+        I : 73,
+        J : 74,
+        K : 75,
+        L : 76,
+        M : 77,
+        N : 78,
+        O : 79,
+        P : 80,
+        Q : 81,
+        R : 82,
+        S : 83,
+        T : 84,
+        U : 85,
+        V : 86,
+        W : 87,
+        X : 88,
+        Y : 89,
+        Z : 90,
+        ENTER : 13,
+        LEFT_ARROW: 37,
+        RIGHT_ARROW: 39,
+        UP_ARROW: 38,
+        DOWN_ARROW: 40,
+        SPACE: 32,
+        SHIFT: 16,
+        TAB: 9
+    };
+
+    module.exports = KeyCodes;
+});
