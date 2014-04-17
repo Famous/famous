0635737ae3ee76574900ce26e11de2f29e96c4c3
Mark Lu <marklu@marklu.net>
2014-04-16 18:16:23 -0700

    Removed unnecessary preventDefault on clicks

--- FastClick.js
+++ FastClick.js
@@ -17,6 +17,7 @@ define(function(require, exports, module) {
     if (!window.CustomEvent) return;
     var clickThreshold = 300;
     var potentialClicks = {};
+    var recentlyDispatched = {};
     window.addEventListener('touchstart', function(event) {
         var timestamp = Date.now();
         for (var i = 0; i < event.changedTouches.length; i++) {
@@ -36,14 +37,23 @@ define(function(require, exports, module) {
             var touch = event.changedTouches[i];
             var startTime = potentialClicks[touch.identifier];
             if (startTime && currTime - startTime < clickThreshold) {
-                event.preventDefault();
                 var clickEvt = new window.CustomEvent('click', {
                     'bubbles': true,
                     'details': touch
                 });
+                recentlyDispatched[currTime] = currTime;
                 event.target.dispatchEvent(clickEvt);
             }
             delete potentialClicks[touch.identifier];
         }
     });
+    window.addEventListener('click', function(event) {
+        var currTime = Date.now();
+        for (var i in recentlyDispatched) {
+            if (currTime - i < clickThreshold) {
+               if (event instanceof window.MouseEvent) event.stopPropagation();
+            }
+            else delete recentlyDispatched[i];
+        }
+    }, true);
 });
