2f03c54f7f3dbdb55d220d142dfc4d3d03dc7390
dmvaldman <dmvaldman@gmail.com>
2014-06-01 16:44:25 -0700

    YUIdoc and getter and setter for progress

--- DrawerLayout.js
+++ DrawerLayout.js
@@ -14,6 +14,29 @@ define(function(require, exports, module) {
     var Transitionable = require('famous/transitions/Transitionable');
     var EventHandler = require('famous/core/EventHandler');
 
+    /**
+     * A layout which will arrange two renderables: a featured content, and a
+     *   concealed drawer. The drawer can be revealed from any side of the
+     *   content (left, top, right, bottom) by dragging the content.
+     *
+     *   A @link{Sync} must be piped in to recieve user input.
+     *
+     *   Events:
+     *     broadcasts: 'open', 'close'
+     *     listens to: 'update', 'end'
+     *
+     * @class DrawerLayout
+     *
+     * @constructor
+     *
+     * @param [options] {Object}                                An object of configurable options
+     * @param [options.side=DrawerLayout.SIDES.LEFT] {Number}   The side of the content the drawer is placed.
+     *                                                          Choice of DrawerLayout.SIDES.LEFT/RIGHT/TOP/BOTTOM
+     * @param [options.drawerLength=0] {Number}                 The default length of the drawer
+     * @param [options.velocityThreshold=0] {Number}            The velocity threshold to trigger a toggle
+     * @param [options.positionThreshold=0] {Number}            The position threshold to trigger a toggle
+     * @param [options.transition=true] {Boolean|Object}        The toggle transition
+     */
     var DrawerLayout = function DrawerLayout(options) {
         this.options = Object.create(DrawerLayout.DEFAULT_OPTIONS);
         this._optionsManager = new OptionsManager(this.options);
@@ -23,6 +46,7 @@ define(function(require, exports, module) {
         this._direction = _getDirectionFromSide(this.options.side);
         this._orientation = _getOrientationFromSide(this.options.side);
         this._isOpen = false;
+        this._cachedLength = 0;
 
         this.drawer = new RenderNode();
         this.content = new RenderNode();
@@ -48,7 +72,7 @@ define(function(require, exports, module) {
 
     DrawerLayout.DEFAULT_OPTIONS = {
         side: DrawerLayout.SIDES.LEFT,
-        defaultDrawerLength : 0,
+        drawerLength : 0,
         velocityThreshold : 0,
         positionThreshold : 0,
         transition : true
@@ -67,10 +91,10 @@ define(function(require, exports, module) {
     function _resolveNodeSize(node) {
         var options = this.options;
         var size;
-        if (options.defaultDrawerLength) size = options.defaultDrawerLength;
+        if (options.drawerLength) size = options.drawerLength;
         else {
             var nodeSize = node.getSize();
-            size = nodeSize ? nodeSize[this._direction] : options.defaultDrawerLength;
+            size = nodeSize ? nodeSize[this._direction] : options.drawerLength;
         }
         return this._orientation * size;
     }
@@ -80,12 +104,14 @@ define(function(require, exports, module) {
 
         var MIN_LENGTH;
         var MAX_LENGTH;
+        this._cachedLength = _resolveNodeSize.call(this, this.drawer);
+
         if (this._orientation === 1){
             MIN_LENGTH = 0;
-            MAX_LENGTH = _resolveNodeSize.call(this, this.drawer);
+            MAX_LENGTH = this._cachedLength;
         }
         else {
-            MIN_LENGTH = _resolveNodeSize.call(this, this.drawer);
+            MIN_LENGTH = this._cachedLength;
             MAX_LENGTH = 0;
         }
 
@@ -100,24 +126,32 @@ define(function(require, exports, module) {
         var position = this._orientation * this.getPosition();
         var options = this.options;
 
-        var MAX_LENGTH = this._orientation * _resolveNodeSize.call(this, this.drawer);
+        var MAX_LENGTH = this._orientation * this._cachedLength;
         var positionThreshold = options.positionThreshold || MAX_LENGTH / 2;
         var velocityThreshold = options.velocityThreshold;
 
         if (options.transition instanceof Object)
             options.transition.velocity = data.velocity;
 
-        if (position === 0 || position === MAX_LENGTH) return;
+        if (position === 0) {
+            this._isOpen = false;
+            return;
+        }
+
+        if (position === MAX_LENGTH) {
+            this._isOpen = true;
+            return;
+        }
 
         var toggle = Math.abs(velocity) > velocityThreshold || (!this._isOpen && position > positionThreshold) || (this._isOpen && position < positionThreshold)
         toggle ? this.toggle() : this.reset();
     }
 
     /**
-     * Patches the HeaderFooterLayout instance's options with the passed-in ones.
+     * Patches the DrawerLayout instance's options with the passed-in ones.
      *
      * @method setOptions
-     * @param {Options} options An object of configurable options for the HeaderFooterLayout instance.
+     * @param options {Object} options
      */
     DrawerLayout.prototype.setOptions = function setOptions(options) {
         this._optionsManager.setOptions(options);
@@ -127,17 +161,33 @@ define(function(require, exports, module) {
         }
     };
 
+    /**
+     * Reveals the drawer with a transition
+     *   Emits an 'open' event when an opening transition has been committed to.
+     *
+     * @method open
+     * @param [transition] {Boolean|Object} transition definition
+     * @param [callback] {Function}         callback
+     */
     DrawerLayout.prototype.open = function open(transition, callback) {
         if (transition instanceof Function) callback = transition;
         if (transition === undefined) transition = this.options.transition;
-        var MAX_LENGTH = _resolveNodeSize.call(this, this.drawer);
-        this.setPosition(MAX_LENGTH, transition, callback);
+        this._cachedLength = _resolveNodeSize.call(this, this.drawer);
+        this.setPosition(this._cachedLength, transition, callback);
         if (!this._isOpen) {
             this._isOpen = true;
             this._eventOutput.emit('open');
         }
     };
 
+    /**
+     * Conceals the drawer with a transition
+     *   Emits a 'close' event when an closing transition has been committed to.
+     *
+     * @method close
+     * @param [transition] {Boolean|Object} transition definition
+     * @param [callback] {Function}         callback
+     */
     DrawerLayout.prototype.close = function close(transition, callback) {
         if (transition instanceof Function) callback = transition;
         if (transition === undefined) transition = this.options.transition;
@@ -148,25 +198,90 @@ define(function(require, exports, module) {
         }
     };
 
+    /**
+     * Sets the position in pixels for the content's displacement
+     *
+     * @method setPosition
+     * @param position {Number}             position
+     * @param [transition] {Boolean|Object} transition definition
+     * @param [callback] {Function}         callback
+     */
     DrawerLayout.prototype.setPosition = function setPosition(position, transition, callback) {
         if (this._position.isActive()) this._position.halt();
         this._position.set(position, transition, callback);
     };
 
+    /**
+     * Gets the position in pixels for the content's displacement
+     *
+     * @method getPosition
+     * @return position {Number} position
+     */
     DrawerLayout.prototype.getPosition = function getPosition() {
         return this._position.get();
     };
 
+    /**
+     * Sets the progress (between 0 and 1) for the content's displacement
+     *
+     * @method setProgress
+     * @param position {Number}             position
+     * @param [transition] {Boolean|Object} transition definition
+     * @param [callback] {Function}         callback
+     */
+    DrawerLayout.prototype.setProgress = function setProgress(progress, transition, callback) {
+        return this._position.set(progress * this._cachedLength, transition, callback);
+    };
+
+    /**
+     * Gets the progress (between 0 and 1) for the content's displacement
+     *
+     * @method getProgress
+     * @return position {Number} position
+     */
+    DrawerLayout.prototype.getProgress = function getProgress() {
+        return this._position.get() / this._cachedLength;
+    };
+
+    /**
+     * Toggles between open and closed states
+     *
+     * @method toggle
+     * @param [transition] {Boolean|Object} transition definition
+     */
     DrawerLayout.prototype.toggle = function toggle(transition) {
         if (this._isOpen) this.close(transition)
         else this.open(transition);
     };
 
+    /**
+     * Resets to last state of being open or closed
+     *
+     * @method reset
+     * @param [transition] {Boolean|Object} transition definition
+     */
     DrawerLayout.prototype.reset = function reset(transition) {
         if (this._isOpen) this.open(transition)
         else this.close(transition);
     };
 
+    /**
+     * Returns if drawer is committed to being open or closed
+     *
+     * @method isOpen
+     * @return {Boolean}
+     */
+    DrawerLayout.prototype.isOpen = function isOpen(transition) {
+        return this._isOpen;
+    };
+
+    /**
+     * Generates a Render Spec from the contents of this component
+     *
+     * @private
+     * @method render
+     * @return {Spec}
+     */
     DrawerLayout.prototype.render = function render() {
         var position = this.getPosition();
 
