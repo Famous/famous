2078845c9c7c543672b98fb503895da4a73d4b98
tim <tim@famo.us>
2014-06-20 11:56:29 -0700

    using Utility.clone to clone options

--- View.js
+++ View.js
@@ -11,6 +11,7 @@ define(function(require, exports, module) {
     var EventHandler = require('./EventHandler');
     var OptionsManager = require('./OptionsManager');
     var RenderNode = require('./RenderNode');
+    var Utility = require('famous/utilities/Utility');
 
     /**
      * Useful for quickly creating elements within applications
@@ -32,7 +33,7 @@ define(function(require, exports, module) {
         EventHandler.setInputHandler(this, this._eventInput);
         EventHandler.setOutputHandler(this, this._eventOutput);
 
-        this.options = Object.create(this.constructor.DEFAULT_OPTIONS || View.DEFAULT_OPTIONS);
+        this.options = Utility.clone(this.constructor.DEFAULT_OPTIONS || View.DEFAULT_OPTIONS);
         this._optionsManager = new OptionsManager(this.options);
 
         if (options) this.setOptions(options);
