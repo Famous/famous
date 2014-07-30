5ca5f4e63b7f0eb9c0e27319d6eef0d2e220bae8
Will Gester <willgester@gmail.com>
2014-07-30 16:36:22 -0700

    Removed size cleanup

--- core/Surface.js
+++ core/Surface.js
@@ -320,7 +320,6 @@ define(function(require, exports, module) {
         target.style.display = 'none';
         target.style.width = '';
         target.style.height = '';
-        // this._size = null;
         _cleanupStyles.call(this, target);
         var classList = this.getClassList();
         _cleanupClasses.call(this, target);
