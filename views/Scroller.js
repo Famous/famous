102c35bebe3f61808a39ac36735492fddc44f4cf
Will Gester <willgester@gmail.com>
2014-07-30 12:11:40 -0700

    Constant time lookup of size

--- views/Scroller.js
+++ views/Scroller.js
@@ -78,8 +78,8 @@ define(function(require, exports, module) {
 
     function _getClipSize() {
         if (this.options.clipSize !== undefined) return this.options.clipSize;
-        if (this._contextSize[this.options.direction] > this._node._.size[this.options.direction]) {
-            return _sizeForDir.call(this, this._node._.size);
+        if (this._contextSize[this.options.direction] > this._node._.cumulativeSizes[this._node._.cumulativeSizes.length - 1][this.options.direction]) {
+            return _sizeForDir.call(this, this._node._.cumulativeSizes[this._node._.cumulativeSizes.length - 1]);
         } else {
             return _sizeForDir.call(this, this._contextSize);
         }
