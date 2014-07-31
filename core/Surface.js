3b7f2d912606e850535bb614dd9ca2a97f23f2a5
tim <tim@famo.us>
2014-07-31 11:43:27 -0700

    guard for pre-commited size

--- core/Surface.js
+++ core/Surface.js
@@ -470,7 +470,7 @@ define(function(require, exports, module) {
      * @return {Array.Number} [x,y] size of surface
      */
     Surface.prototype.getSize = function getSize() {
-        return this._size;
+        return this._size ? this._size : this.size;
     };
 
     /**
