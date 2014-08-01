ab54001693abe0db04673644509f525f1187b362
Myles Borins <myles.borins@gmail.com>
2014-08-01 10:56:10 -0700

    Revert "allow module to load without a body"

--- core/ElementOutput.js
+++ core/ElementOutput.js
@@ -12,7 +12,7 @@ define(function(require, exports, module) {
     var EventHandler = require('./EventHandler');
     var Transform = require('./Transform');
 
-    var usePrefix = document.createElement('div').style.webkitTransform !== undefined;
+    var usePrefix = document.body.style.webkitTransform !== undefined;
     var devicePixelRatio = window.devicePixelRatio || 1;
 
     /**
