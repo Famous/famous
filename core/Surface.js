ba6bd9706edb8d0cadeefc16de27751d2f334c19
MajorBreakfast <mail@josefbrandl.de>
2014-08-02 12:57:43 +0200

    [DOC] CSS classes are set on target div

--- core/Surface.js
+++ core/Surface.js
@@ -21,7 +21,7 @@ define(function(require, exports, module) {
      *
      * @param {Object} [options] default option overrides
      * @param {Array.Number} [options.size] [width, height] in pixels
-     * @param {Array.string} [options.classes] CSS classes to set on inner content
+     * @param {Array.string} [options.classes] CSS classes to set on target div
      * @param {Array} [options.properties] string dictionary of HTML attributes to set on target div
      * @param {string} [options.content] inner (HTML) content of surface
      */
