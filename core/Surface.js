c621d2674f73a6cc815554333b1dbf14f6e7b156
Will Gester <willgester@gmail.com>
2014-08-05 14:08:42 -0700

    handles true size changes for getSize

--- core/Surface.js
+++ core/Surface.js
@@ -327,7 +327,9 @@ define(function(require, exports, module) {
                 if (this._contentDirty || this._stylesDirty || this._classesDirty || this._size[0] === 0) {
                     var width = target.clientWidth;
                     if (this._size && this._size[0] !== width) {
+                        this._size[0] = width;
                         this._eventOutput.emit('trueSizeChange');
+                        this._sizeDirty = true;
                     }
                     size[0] = width;
                 } else {
@@ -339,7 +341,9 @@ define(function(require, exports, module) {
                 if (this._contentDirty || this._stylesDirty || this._classesDirty || this._size[1] === 0) {
                     var height = target.clientHeight;
                     if (this._size && this._size[1] !== height) {
+                        this._size[1] = height;
                         this._eventOutput.emit('trueSizeChange');
+                        this._sizeDirty = true;
                     }
                     size[1] = height;
                 } else {
