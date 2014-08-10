43dffffc7ba431016b87562c1120acfafb29996b
Peter Kiers <pkiers@MacBook-Pro-van-Peter.local>
2014-08-10 14:42:31 -0700

    Reset origin dirty flag on setup

--- core/Surface.js
+++ core/Surface.js
@@ -241,6 +241,7 @@ define(function(require, exports, module) {
         this._classesDirty = true;
         this._sizeDirty = true;
         this._contentDirty = true;
+        this._originDirty = true;
         this._transformDirty = true;
     };
 
