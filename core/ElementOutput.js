de3507092e3047f8eab5a90591e09ab22e6d35e7
Eric Schoffstall <contra@wearefractal.com>
2014-08-21 01:11:18 -0700

    fix vendor prefix checking

--- core/ElementOutput.js
+++ core/ElementOutput.js
@@ -12,7 +12,7 @@ define(function(require, exports, module) {
     var EventHandler = require('./EventHandler');
     var Transform = require('./Transform');
 
-    var usePrefix = document.body.style.webkitTransform !== undefined;
+    var usePrefix = ('webkitTransform' in document.documentElement);
     var devicePixelRatio = window.devicePixelRatio || 1;
 
     /**
