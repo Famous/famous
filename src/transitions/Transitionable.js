2f3cc50a44e791d89cf04956d998f09784c46470
Alexander Gugel <alexander.gugel@gmail.com>
2014-11-28 17:35:42 -0800

    Fix doc; Make API more consistent

--- src/transitions/Transitionable.js
+++ src/transitions/Transitionable.js
@@ -126,6 +126,7 @@ define(function(require, exports, module) {
      *    instantaneous.
      * @param {function()=} callback Zero-argument function to call on observed
      *    completion (t=1)
+     * @return {Transitionable} this
      */
     Transitionable.prototype.set = function set(endState, transition, callback) {
         if (!transition) {
@@ -168,6 +169,7 @@ define(function(require, exports, module) {
      * @param {number} duration delay time (ms)
      * @param {function} callback Zero-argument function to call on observed
      *    completion (t=1)
+     * @return {Transitionable} this
      */
     Transitionable.prototype.delay = function delay(duration, callback) {
         var endValue;
@@ -218,6 +220,8 @@ define(function(require, exports, module) {
      * Halt transition at current state and erase all pending actions.
      *
      * @method halt
+     *
+     * @return {Transitionable} this
      */
     Transitionable.prototype.halt = function halt() {
         return this.set(this.get());
