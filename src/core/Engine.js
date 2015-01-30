ba4636c4a1bdb8214ae32a966bcc60ff4b865249
Joseph Carroll <jdsalingerjr@gmail.com>
2015-01-29 21:21:59 -0500

    Please let me pass lint

--- src/core/Engine.js
+++ src/core/Engine.js
@@ -78,8 +78,8 @@ define(function(require, exports, module) {
         var currentTickQueue = nextTickQueue;
         nextTickQueue = undefined;
 
-        var i = 0,
-            l = 0;
+        var i = 0;
+        var l = 0;
 
         // empty the queue
         if (currentTickQueue) {
